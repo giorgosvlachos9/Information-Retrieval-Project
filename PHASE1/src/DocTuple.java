7041069b9690a5395c4580756123e6ad54be5b8f
giorgosvlachos9 <giorgosvlachos111@gmail.com>
2024-06-28 16:23:31 +0300

    Files commited

---
+++ PHASE1/src/DocTuple.java
@@ -0,0 +1,16 @@
+public class DocTuple {
+
+    private String code;
+    private String text;
+    public DocTuple(String code, String text) {
+        this.code = code;
+        this.text = text;
+    }
+
+    public String getCode() { return code; }
+
+    public String getText() {
+        return text;
+    }
+
+}
